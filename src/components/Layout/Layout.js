b9ac4ec54e5c56be51ea70977888786a66aed846
Jesuloba <jesulobajohn@gmail.com>
2020-11-06 04:08:21 +0100

    Working on the Toolbar and SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
-  <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
+	<Aux>
+		<Toolbar />
+		<SideDrawer />
+		<main className={classes.Content}>{props.children}</main>
+	</Aux>
 );
 
 export default layout;
