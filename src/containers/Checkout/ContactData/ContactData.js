79a898ad35c3753210d4d074f82a15805da9db18
Jesuloba <jesulobajohn@gmail.com>
2020-11-13 02:37:00 +0100

    Adding Overall form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -90,8 +90,11 @@ class ContactData extends Component {
 					],
 				},
 				value: "",
+				validation: {},
+				valid: true,
 			},
 		},
+		formIsValid: false,
 		loading: false,
 	};
 
@@ -128,6 +131,10 @@ class ContactData extends Component {
 	checkValidity(value, rules) {
 		let isValid = true;
 
+		if (!rules) {
+			return true;
+		}
+
 		if (rules.required) {
 			isValid = value.trim() !== "" && isValid;
 		}
@@ -158,7 +165,17 @@ class ContactData extends Component {
 		updatedFormElement.touched = true;
 		updatedOrderForm[inputIdentifier] = updatedFormElement;
 		// console.log(updatedFormElement);
-		this.setState({ orderForm: updatedOrderForm });
+
+		let formIsValid = true;
+		for (let inputIdentifier in updatedOrderForm) {
+			formIsValid =
+				updatedOrderForm[inputIdentifier].valid && formIsValid;
+		}
+
+		this.setState({
+			orderForm: updatedOrderForm,
+			formIsValid: formIsValid,
+		});
 	};
 
 	render() {
@@ -186,7 +203,9 @@ class ContactData extends Component {
 						}
 					/>
 				))}
-				<Button btnType="Success">ORDER</Button>
+				<Button btnType="Success" disabled={!this.state.formIsValid}>
+					ORDER
+				</Button>
 			</form>
 		);
 
