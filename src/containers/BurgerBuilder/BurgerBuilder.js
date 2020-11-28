a8b93f4eff542bcb3a457547408162fe51293de8
Jesuloba <jesulobajohn@gmail.com>
2020-11-28 16:13:18 +0100

    Forwarding Unauthenticated Users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,7 +33,11 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseHandler = () => {
-		this.setState({ purchasing: true });
+		if (this.props.isAuthenticated) {
+			this.setState({ purchasing: true });
+		} else {
+			this.props.history.push("/auth");
+		}
 	};
 
 	purchaseCancelHandler = () => {
@@ -73,6 +77,7 @@ class BurgerBuilder extends Component {
 						purchasable={this.updatePurchaseState(this.props.ings)}
 						price={this.props.price}
 						order={this.purchaseHandler}
+						isAuth={this.props.isAuthenticated}
 					/>
 				</Aux>
 			);
@@ -106,6 +111,7 @@ const mapStateToProps = (state) => {
 		ings: state.burgerBuilder.ingredients,
 		price: state.burgerBuilder.totalPrice,
 		error: state.burgerBuilder.error,
+		isAuthenticated: state.auth.token !== null,
 	};
 };
 
