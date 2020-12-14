fc1954889335c29369ede826a0f0b00027f2a445
Jesuloba <jesulobajohn@gmail.com>
2020-12-14 23:50:13 +0100

    updated other components to Hooks

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,46 +1,33 @@
-import { Component } from "react";
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-class OrderSummary extends Component {
-	componentDidUpdate() {
-		// console.log("[OrderSummary.js] Did update!");
-	}
-
-	render() {
-		const ingredientSummary = Object.keys(this.props.ingredients).map(
-			(igKey, i) => {
-				return (
-					<li key={igKey + i}>
-						<span style={{ textTransform: "capitalize" }}>
-							{igKey}
-						</span>
-						: {this.props.ingredients[igKey]}
-					</li>
-				);
-			}
-		);
-
+const OrderSummary = (props) => {
+	const ingredientSummary = Object.keys(props.ingredients).map((igKey, i) => {
 		return (
-			<Aux>
-				<h3>Your Order</h3>
-				<p>A delicious burger with the following ingredients:</p>
-				<ul>{ingredientSummary}</ul>
-				<p>
-					<strong>
-						Total price: ${this.props.totalPrice.toFixed(2)}
-					</strong>
-				</p>
-				<p>Continue to Checkout?</p>
-				<Button btnType={"Danger"} clicked={this.props.cancel}>
-					CANCEL
-				</Button>
-				<Button btnType={"Success"} clicked={this.props.continue}>
-					CONTINUE
-				</Button>
-			</Aux>
+			<li key={igKey + i}>
+				<span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+				{props.ingredients[igKey]}
+			</li>
 		);
-	}
-}
+	});
+
+	return (
+		<Aux>
+			<h3>Your Order</h3>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>{ingredientSummary}</ul>s
+			<p>
+				<strong>Total price: ${props.totalPrice.toFixed(2)}</strong>
+			</p>
+			<p>Continue to Checkout?</p>
+			<Button btnType={"Danger"} clicked={props.cancel}>
+				CANCEL
+			</Button>
+			<Button btnType={"Success"} clicked={props.continue}>
+				CONTINUE
+			</Button>
+		</Aux>
+	);
+};
 
 export default OrderSummary;
